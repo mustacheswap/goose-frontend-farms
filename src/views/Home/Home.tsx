9b046943bd8b20982c10e609a726a4d0697b2f13
mustacheswap <83590343+mustacheswap@users.noreply.github.com>
2021-05-11 14:24:31 +0200

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -11,7 +11,6 @@ import TwitterCard from './components/TwitterCard'
 
 const Hero = styled.div`
   align-items: center;
-  background-image: url('/images/egg/3.png');
   background-repeat: no-repeat;
   background-position: top center;
   display: flex;
